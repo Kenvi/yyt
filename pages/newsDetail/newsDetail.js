042f61cd4e756c9c0877c1d8e952b474e0e064a9
Kenvi <604113530@qq.com>
2017-05-30 21:48:02 +0800

    <资讯>资讯详情页接口对接（微信无法查看照片）

--- pages/newsDetail/newsDetail.js
+++ pages/newsDetail/newsDetail.js
@@ -27,11 +27,49 @@ Page({
     }else{
       app.api.getInformationContent(opts.id)
         .then(function (content) {
+          // let imgList = [] , indexArr = []
+          // if(content.informationContentList){
+          //   content.informationContentList.forEach(function (item,index) {
+          //
+          //     if(item.type === 1){
+          //       imgList.push(that.getImageOpts(item,index,item.content))
+          //       indexArr.push(index)
+          //     }
+          //   })
+          //   Promise.all(imgList)
+          //     .then(function (res) {
+          //       res.forEach(function (item) {
+          //         content.informationContentList[item.index] = item
+          //       })
+          //       return content
+          //     })
+          //     .then(function (final) {
+          //       that.setData({
+          //         newsDetail:final,
+          //         detailTitle:opts.title
+          //       })
+          //     })
+          // }
           that.setData({
             newsDetail:content,
             detailTitle:opts.title
           })
         })
+
+
     }
+  },
+  getImageOpts:function (opts,index,path) {
+    const info = wx.getSystemInfoSync()
+    let img = new Image()
+    return new Promise(function (resolve,reject) {
+      img.src= path
+      img.onload = function (img) {
+        opts.index = index
+        // opts.width = img.path[0].width
+        opts.height = img.path[0].height / img.path[0].width * (info.windowWidth-30)
+        resolve(opts)
+      }
+    })
   }
 })
