d6466c0a1e400e1c4e477769252de12a467b6d76
Kenvi <604113530@qq.com>
2017-05-30 11:25:52 +0800

    <资讯>添加资讯列表及内容接口

--- pages/newsList/newsList.js
+++ pages/newsList/newsList.js
@@ -7,10 +7,56 @@ const objAssign = require('../../util/objectAssign')
 import headTitle from  '../../components/headTitle/headTitle.js'
 Page({
   data:{
-
+    informationList:[]
   },
   onLoad:function () {
     const that = this
     objAssign(that, headTitle)
+    that.getInformationList()
+      .then(function (list) {
+        that.setData({
+          informationList:list
+        })
+      })
+    that.getInformationContent(900)
+  },
+  getInformationList:function () {
+    return new Promise(function (resolve,reject) {
+      wx.request({
+        url: 'https://www.emtsos.com/emMiniApi.do',
+        data: {
+          method:'getInformationList',
+        },
+        success:function (res) {
+          if(res.data.ret === 1){
+            resolve(res.data.data.informationList)
+          }
+        },
+        fail:function (err) {
+          console.log(err)
+          reject(err)
+        }
+      })
+    })
+  },
+  getInformationContent:function (informationid) {
+    return new Promise(function (resolve,reject) {
+      wx.request({
+        url: 'https://www.emtsos.com/emMiniApi.do',
+        data: {
+          method:'getInformationContent',
+          informationid:informationid
+        },
+        success:function (res) {
+          if(res.data.ret === 1){
+            resolve(res.data.data)
+          }
+        },
+        fail:function (err) {
+          console.log(err)
+          reject(err)
+        }
+      })
+    })
   }
 })
