3bcd9228a673966f2d2665a7ee42aae8b1491ff1
Kenvi <604113530@qq.com>
2017-07-05 22:34:55 +0800

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,15 @@ const rename  = require('gulp-rename');
 gulp.task('sass', function () {
 
   return gulp.src('./pages/**/*.scss')
-  	.pipe(changed('./pages/**/*.scss'))
 
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(rename((path) => path.extname = '.wxss'))
     .pipe(gulp.dest('./pages'));
 });
  
 gulp.task('sass:watch', function () {
   gulp.watch('./pages/**/*.scss', ['sass']);
+  gulp.watch('./components/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass:watch']);
